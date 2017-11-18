0740e6b306d78d6584352516e47c3c8e945cd3b5
Sally Zasloff <szasloff@gmail.com>
2017-11-18 22:42:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
       students = []
     index_page.css("div.roster-cards-container").each do |card|
         card.css(".student-card").each do |student|
-          student_name = (".student-name").text
+          student_name = student.css(".student-name").text
     # binding.pry
       end
     end
